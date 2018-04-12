6d3d7b76ddeb0101d3f5452e8f26bb03d9ff8d4e
KimieJoe <kimiejoeart@gmail.com>
2018-04-12 10:27:59 -0400

    first commit

---
+++ db/migrate/20180412131928_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :string
+      t.integer :age
+      t.string :zip
+
+      t.timestamps
+    end
+  end
+end
